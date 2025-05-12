5f22e7128e115e374a7964cc0e1a7b03f4a9b0e4
Seba Mamby <s.mamby@gmail.com>
2025-05-12 00:18:58 -0300

    depurado, funcionando 100%

--- reset.sh
+++ reset.sh
@@ -1,21 +1,37 @@
 !/bin/bash
 
 
-echo "Ingrese el nombre de la imagen (ej: mi-app):"
-read imagene
-echo "Ingrese la url imagen (ej: . o ./public):"
-read url
-echo "Ingrese puertos: (ej: '8080:8080'):"
-read ports
+# echo "Ingrese el nombre de la imagen (ej: mi-app):"
+# read imagen
+# echo "Ingrese la url imagen (ej: . o ./public):"
+# read url
+# echo "Ingrese puertos: (ej: '8080:8080'):"
+# read ports
 
-docker stop $imagene
+# Verificar que se proporcionen los tres argumentos necesarios
+if [[ $# -ne 3 ]]; then
+  echo "Uso: $0 <nombre_imagen> <url_imagen> <puertos>"
+  echo "Ejemplo: $0 mi-app . '8080:8080'"
+  exit 1
+fi
 
-docker rm $imagene
+# Asignar los parámetros posicionales a variables
+imagen="$1"
+url="$2"
+ports="$3"
 
-docker build -t $imagene $url
+echo "Nombre de la imagen: $imagen"
+echo "URL del dockerfile: $url"
+echo "Puertos: $ports"
+
+docker stop $imagen
+
+docker rm $imagen
+
+docker build -t $imagen $url
 docker run -d \
-  --name $imagene \
+  --name $imagen \
   -p $ports \
   --network nw_pfo2 \
-  $imagene
+  $imagen
 
